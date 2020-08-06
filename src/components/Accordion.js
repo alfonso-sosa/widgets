7f0910eec6fdc1eaae173dec052e01c403e8006d
alfonso-sosa <alfonso.sosa@hulu.com>
2020-08-05 21:30:30 -0700

    add helper function

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,26 @@
 import React, { Fragment } from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map(item => {
-    return <Fragment key={item.title}>
-      <div className='title active'>
-        <i className='dropdown icon' />
-        {item.title}
-      </div>
-      <div className='content active'>
-        <p>{item.content}</p>
-      </div>
-    </Fragment>
+
+  const onTitleClick = (index) => {
+    console.log(`Title clicked`, index);
+  }
+
+  const renderedItems = items.map((item, index) => {
+    return (
+      <Fragment key={item.title}>
+        <div 
+          className='title active'
+          onClick={() => onTitleClick(index)}
+        >
+          <i className='dropdown icon' />
+          {item.title}
+        </div>
+        <div className='content active'>
+          <p>{item.content}</p>
+        </div>
+      </Fragment>
+    )
   });
   return <div className='ui styled accordion'>{renderedItems}</div>
 }
