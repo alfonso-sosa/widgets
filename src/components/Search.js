4a2738ae3ef057f6e02f868c9b40aa92b4b0510c
alfonso-sosa <alfonso.sosa@hulu.com>
2020-08-08 11:30:30 -0700

    setup axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
   useEffect(() => {
-    console.log('I run only when term changed')
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+    }
+    search();
   }, [term]);
   return (
     <div>
